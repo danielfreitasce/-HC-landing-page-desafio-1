bc1737f32f7fafe5b18e5703686fa746d92e3517
Daniel Freitas <contatodanielfreitasce@gmail.com>
2021-07-15 20:56:24 -0300

    Alteração do arquivo js e html

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-/*function armazenar(){
+function armazenar(){
 
     if(localStorage.contador){
         localStorage.contador = Number(localStorage.contador)+1;
@@ -10,21 +9,6 @@
 
        let inputEmail = document.getElementById('email').value;
        localStorage.setItem('email_'+localStorage.contador, inputEmail);
+    alert('Email cadastrado com sucesso!')
 
-}*/
-
-function armazenar() {
-
-    if (localStorage.contador) {
-        localStorage.contador = Number(localStorage.contador) + 1;
-    }
-    else {
-        localStorage.contador = 1;
-    }
-
-    var inputEmail = document.getElementById('email').value;
-    
-    localStorage.setItem('email_' + localStorage.contador, inputEmail);
-    let $ = document.querySelector;
-    $("#cadastro-email").replaceWith("<div id='Elemento-3'>Obrigado por se inscrever</div>");
 }
