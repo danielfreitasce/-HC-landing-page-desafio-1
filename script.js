01acb3c3663a333579db21801d1c9b04e82019f5
Daniel Freitas <contatodanielfreitasce@gmail.com>
2021-07-16 09:57:02 -0300

    Tetanto desenvolver layout e validação de email básica adicionada

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-function armazenar(){
+function armazenar() {
 
-    if(localStorage.contador){
-        localStorage.contador = Number(localStorage.contador)+1;
+    let inputEmail = document.getElementById('email').value;
+
+    if ( (inputEmail === "") || (!(validaEmail(inputEmail))) ) {
+        swal({
+            title: "Oxe",
+            text: "Coloque seu email",
+            icon: "warning",
+        });
+
+    } else {
+
+
+        if (localStorage.contador) {
+            localStorage.contador = Number(localStorage.contador) + 1;
+        }
+        else {
+            localStorage.contador = 1;
+        }
+
+        localStorage.setItem('email_' + localStorage.contador, inputEmail);
+        swal({
+            title: "Só o mí disbuiado",
+            text: "Você se cadastrou!",
+            icon: "success",
+        });
+
     }
-    else{
-        localStorage.contador=1;
+
+
+    function validaEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
     }
-
-       let inputEmail = document.getElementById('email').value;
-       localStorage.setItem('email_'+localStorage.contador, inputEmail);
-    alert('Email cadastrado com sucesso!')
-
 }
+
+
+
